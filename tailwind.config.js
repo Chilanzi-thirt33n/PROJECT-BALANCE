ac46341e286de09f5d97cf4eef2632a7090032ae
chilanzi <davidchilanzijilliamdark@gmail.com>
2023-02-09 22:12:16 +0200

    index update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        myWhite: "#F5F5F5",
+        myGreen: "#86BC42",
+        myDarkestGreen: "#1A2223",
+        myDarkGreen: "#283739",
+        myGray: "#7c7c7c",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
